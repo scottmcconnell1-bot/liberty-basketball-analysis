831da8a16d9481536ea7ef075f16e81636229031
monk-admin <monk-admin@users.noreply.github.com>
2026-05-22 02:09:34 -0600

    backup: 2026-05-22-0209 —  57 files changed, 10019 insertions(+), 1087 deletions(-)

--- helpers.py
+++ helpers.py
@@ -681,7 +681,6 @@ def get_db():
     if "db" not in g:
         g.db = sqlite3.connect(
             current_app.config["DATABASE"],
-            detect_types=sqlite3.PARSE_DECLTYPES,
             timeout=10,
         )
         g.db.row_factory = sqlite3.Row
